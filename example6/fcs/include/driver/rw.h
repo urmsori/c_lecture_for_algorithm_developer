42b07bef25cdba9b4a1853bf235cfba749122e90
jykim <jykim@satreci.com>
2021-11-30 12:01:02 +0900

    example6

---
+++ example6/fcs/include/driver/rw.h
@@ -0,0 +1,7 @@
+#ifndef FCS_INCLUDE_DRIVER_RW_H_
+#define FCS_INCLUDE_DRIVER_RW_H_
+
+void rw_on(void);
+void rw_off(void);
+
+#endif // FCS_INCLUDE_DRIVER_RW_H_
---
+++ example6/fcs/source/driver/rw.c
@@ -0,0 +1,11 @@
+#include "driver/rw.h"
+
+#include <stdio.h>
+
+void rw_on(void) {
+  printf("Turn On RW\n");
+}
+
+void rw_off(void) {
+  printf("Turn Off RW\n");
+}
